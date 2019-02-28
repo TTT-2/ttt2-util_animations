b74e422d12169834cc22daa3218d1f15f1c62803
Alf Müller <4lf-mueller@gmx.de>
2019-02-28 19:48:19 +0100

    Using the new TTT2 build-in avatar downloading

--- lua/autorun/hudanim_init.lua
+++ lua/autorun/hudanim_init.lua
@@ -2,11 +2,9 @@ if SERVER then
 	AddCSLuaFile()
 	AddCSLuaFile("animations/client/calculations/cubic_bezier.lua")
 	AddCSLuaFile("animations/client/role_anim.lua")
-	AddCSLuaFile("animations/client/libs/web_image.lua")
 	AddCSLuaFile("animations/client/connect_anim.lua")
-	AddCSLuaFile("animations/cl_init.lua")
-
-	include("animations/init.lua")
 else
-	include("animations/cl_init.lua")
+	include("animations/client/calculations/cubic_bezier.lua")
+	include("animations/client/role_anim.lua")
+	include("animations/client/connect_anim.lua")
 end
